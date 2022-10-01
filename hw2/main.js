824bd8e5085b537ffc17fc913c260af091c35764
liuyu <queeniiee02@gmail.com>
2022-10-02 01:24:44 +0800

    remove leave bottun from you

--- hw2/main.js
+++ hw2/main.js
@@ -35,7 +35,9 @@ class card {
             let mute = document.createElement("div");
             mute.className = "mute";
             mute.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20"><path d="M14.604 12.479 13.5 11.375q.229-.437.365-.906Q14 10 14 9.5h1.5q0 .792-.229 1.552t-.667 1.427Zm-2.229-2.229L7.5 5.375V4.5q0-1.042.729-1.771Q8.958 2 10 2q1.042 0 1.771.729.729.729.729 1.771v5q0 .188-.031.375t-.094.375ZM9.25 17v-2.062q-2.062-.25-3.406-1.803Q4.5 11.583 4.5 9.5H6q0 1.667 1.167 2.833Q8.333 13.5 10 13.5q.708 0 1.344-.271.635-.271 1.177-.729l1.083 1.083q-.604.521-1.333.886-.729.364-1.521.469V17ZM16 18.125 1.875 3.979l1.063-1.062 14.124 14.145Z"/></svg>';
-            block_b1.append(remove_bottom);
+            if (name != "you") {
+                block_b1.append(remove_bottom);
+            }
             block_b1.append(mute);
 
             let block_b2 = document.createElement("div");
@@ -66,6 +68,7 @@ class card {
             node.appendChild(block_b1);
             node.appendChild(block_b2);
             node.appendChild(block_b3);
+
             if (pin_self) {
                 var m = document.getElementById("main_container");
                 content.classList.add("main_content");
