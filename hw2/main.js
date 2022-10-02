e28930e4e4c3aa9d1ee4c9f805f1e10bbaf9e8ce
liuyu <queeniiee02@gmail.com>
2022-10-03 01:07:56 +0800

    primary you remove button display none

--- hw2/main.js
+++ hw2/main.js
@@ -37,6 +37,7 @@ class card {
             mute.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20"><path d="M14.604 12.479 13.5 11.375q.229-.437.365-.906Q14 10 14 9.5h1.5q0 .792-.229 1.552t-.667 1.427Zm-2.229-2.229L7.5 5.375V4.5q0-1.042.729-1.771Q8.958 2 10 2q1.042 0 1.771.729.729.729.729 1.771v5q0 .188-.031.375t-.094.375ZM9.25 17v-2.062q-2.062-.25-3.406-1.803Q4.5 11.583 4.5 9.5H6q0 1.667 1.167 2.833Q8.333 13.5 10 13.5q.708 0 1.344-.271.635-.271 1.177-.729l1.083 1.083q-.604.521-1.333.886-.729.364-1.521.469V17ZM16 18.125 1.875 3.979l1.063-1.062 14.124 14.145Z"/></svg>';
             if (name == "you") {
                 remove_bottom.id = "main_remove";
+                remove_bottom.classList.add("you_remove");
             }
             block_b1.append(remove_bottom);
             block_b1.append(mute);
@@ -74,7 +75,7 @@ class card {
                 var m = document.getElementById("main_container");
                 content.classList.add("main_content");
                 m.appendChild(node);
-                document.getElementById("main_remove").style.display = "visible";
+                //document.getElementsByClassName("you_remove")[0].style.display = "visible";
             }
             else {
                 var s = document.getElementById("side_container");
@@ -132,7 +133,7 @@ class card {
         };
         this.node = make_node();
 
-        console.log("construct ", this.name, " == ", this.self);
+        console.log("construct ", this.name);
     }
     swap_pined = () => {
         console.log("swap pined");
@@ -141,7 +142,6 @@ class card {
             var tmp_path = MAIN.path;
             MAIN.name = this.name;
             MAIN.path = this.path;
-            //this.clean();
             var old_main_to_side = new card(false, tmp_name, tmp_path);
             let main_txt = document.getElementsByClassName("main_content");
             main_txt[0].innerHTML = MAIN.name; 
@@ -236,6 +236,19 @@ function if_pined() {
         s.style.width = "100%";
         console.log("unpined");
     }
+    if (MAIN.name == "you") {
+        console.log("main_remove none");
+        var you_none = document.getElementsByClassName("you_remove");
+        you_none[0].style.display = "none"; 
+    }
+    else {
+        var main_visible = document.getElementById("main_remove");
+        main_visible.classList.remove("you_remove");
+        main_visible.style.display = "block";
+        var you_none = document.getElementsByClassName("you_remove");
+        you_none[0].style.display = "none"; 
+
+    }
 }
 
 //start
@@ -262,6 +275,7 @@ function update_time() {
 setInterval(update_time, 1000);
 
 MAIN = new card(true, "you", "./");
+if_pined();
 //createYou();
 
 //end
