14257eda2e2d68f0727d27a1829f68e5706c8462
ly <queeniiee02@gmail.com>
2022-10-30 13:41:42 +0800

    little change in axios.js

--- hw5/frontend/src/axios.js
+++ hw5/frontend/src/axios.js
@@ -1,17 +1,19 @@
 import axios from 'axios';
-const instance = axios.create({baseURL: '...'});
+const instance = axios.create({baseURL: 'http://localhost:4000/api/guess'}); 
 const startGame = async () => {
     const {data:{msg} } = await instance.post('/start');
     return msg;
 }
 const guess = async(number) => {
-   
+    try {
+    
+        return msg
+    }
+    catch (error) {
+
+    }
    
 }   
-   
-   
-   
-   
    
 const restart = 0;
 export {startGame, guess, restart};
