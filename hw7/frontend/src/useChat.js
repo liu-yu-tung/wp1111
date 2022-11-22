03c64b1e86fa2b68864f0a35cb08d028b18629bd
liuyu <queeniiee02@gmail.com>
2022-11-22 13:55:52 +0800

    frontend status display fixed

--- hw7/frontend/src/useChat.js
+++ hw7/frontend/src/useChat.js
@@ -4,18 +4,33 @@ const useChat = () => {
   const [messages, setMessages] = useState([]);
   const [status, setStatus] = useState({});
   const client = new WebSocket('ws://localhost:4000')
+  client.onmessage = (byteString) => {
+    const {data} = byteString
+    const [task, payload] = JSON.parse(data)
+    switch (task) {
+      case "output": {
+        setMessages(() => [...messages, ...payload])
+        break
+      }
+      case "status": {
+        setStatus(payload)
+        break
+      }
+      default: break 
+    }
+  }
   const sendData = async (data) => {
     await client.send(JSON.stringify(data))
-    console.log("send " + JSON.stringify(data))
+    //console.log("send " + JSON.stringify(data))
   }
   const sendMessage = (payload) => { 
     sendData(["input", payload])
     setMessages([...messages, {name: payload.name, body: payload.body}]);
     setStatus({
       type: "success",
-      payload: "Message sent."
+      msg: "Message sent."
     })
-    console.log(payload);
+    console.log("payload: " + payload);
   }
   return {
     status, messages, sendMessage
