2937330f91e47ef3ec3e4dc396e826d2bbfe4251
liuyu <queeniiee02@gmail.com>
2022-11-30 00:04:45 +0800

    new

--- hack2/backend/src/routes/comment.js
+++ hack2/backend/src/routes/comment.js
@@ -27,8 +27,6 @@ exports.GetCommentsByRestaurantId = async (req, res) => {
     //    message: 'error'
     //    contents: []
     // }
-    console.log("GetCommentsByRestaurantId called")
-    console.log(id)
     const msg = await Comment.find({"restaurantId":id}).exec((err,data) => {
         if (!err) {
             res.status(200).send(data)
@@ -43,7 +41,6 @@ exports.CreateComment = async (req, res) => {
     /*******    NOTE: DO NOT MODIFY   *******/
     const body = req.body
     /****************************************/
-    console.log(body)
     const {restaurantId, name, rating, content} = body.params
     const newC = new Comment({restaurantId: restaurantId, name: name, rating: rating, content: content})
     res.status(200).send({message: "yes"})
