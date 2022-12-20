5219b11656015af5670e94f80c372376dcd897ab
liuyu <queeniiee02@gmail.com>
2022-12-20 09:24:54 +0800

    mongo add

--- hack3/backend/src/mongo.js
+++ hack3/backend/src/mongo.js
@@ -7,7 +7,12 @@ mongoose.set("strictQuery", true);
 
 async function connect() {
   // TODO 1 Connect to your MongoDB and call dataInit()
+  mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true, })
+    .then((res) => console.log("mongo db connection created"));
 
+  mongoose.connection.on('error',console.error.bind(console, 'connection error:')); 
   // TODO 1 End
 }
 
