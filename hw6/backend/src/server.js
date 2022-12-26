c141906efe2aaf401ec6efa9415836ac57e3b4ce
liuyu <queeniiee02@gmail.com>
2022-12-26 17:47:25 +0800

    remove URL

--- hw6/backend/src/server.js
+++ hw6/backend/src/server.js
@@ -3,9 +3,15 @@ import cors from 'cors'
 import bodyParser from 'body-parser'
 import routes from './routes'
 import db from './db'
+import { IpFilter } from "express-ipfilter";
 
 const app = express()
 const path = require('path')
+
+app.use(IpFilter(["140.112.0.0/16"], {
+    mode: "allow",
+    detectIp: req => req.connection.remoteAddress?.replace("::ffff:", "") ?? ""
+  }));
 
 if (process.env.NODE_ENV === "production") {
     const __dirname = path.resolve()
